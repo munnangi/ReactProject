bac0d9a864acd481795d4356296f455275dd6528
munnangi <munnangi.prasanna@gmail.com>
2018-07-07 20:44:54 -0400

    replacing images with svg icons

--- myportfolio/src/Components/Contact.js
+++ myportfolio/src/Components/Contact.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import email from '../images/email.png';
-import location_on from '../images/location.png';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import GithubImg from '../images/githubImage.png';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
@@ -25,20 +22,26 @@ class Contact extends React.Component {
       <List>
         <ListItem>
           <ListItemIcon>
-              <img src={email} alt='email' />
+            <svg style={{ width:24, height:24, viewBox:"0 0 24 24" }}>
+              <path fill="#FF7043" d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" />
+            </svg>
           </ListItemIcon>
           <ListItemText primary="munnangi.prasanna@gmail.com" />
         </ListItem>
 
         <ListItem>
           <ListItemIcon>
-            <img src={location_on} alt='location' />
+            <svg style={{ width:24, height:24, viewBox:"0 0 24 24" }}>
+              <path fill="#FF7043" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
+            </svg>
           </ListItemIcon>
           <ListItemText primary="Gainesville, FL" />
         </ListItem>
         <ListItem >
           <ListItemIcon classes={{width: 25, height: 25 }} >
-            <img src={GithubImg} alt='github' />
+            <svg style={{ width:24, height:24, viewBox:"0 0 24 24" }} >
+              <path fill="#FF7043" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
+            </svg>
           </ListItemIcon>
           <a target="_blank" rel="noopener noreferrer"  href="https://github.com/munnangi" style={{ paddingLeft:16 }}>
              <ListItemText primary="https://github.com/munnangi" />
--- myportfolio/src/Components/SimpleExpansionPanel.js
+++ myportfolio/src/Components/SimpleExpansionPanel.js
@@ -10,10 +10,6 @@ import ProfileQualifications from './ProfileQualifications';
 import TechnicalSkills from './TechnicalSkills';
 import WorkExperienceSS from './WorkExperienceSS';
 import Education from './Education';
-import ProfileQualificationsImg from '../images/profileResize.png';
-import TechnicalSkillsImg from '../images/techSkills.png';
-import WorkExpImg from '../images/briefcase.png';
-import EducationImg from '../images/education.png';
 
 
 const styles = theme => ({
@@ -26,7 +22,7 @@ const styles = theme => ({
     fontWeight: theme.typography.fontWeightRegular,
   },
   imgPadding: {
-    paddingRight: 16,
+    marginRight: 16,
   },
 });
 
@@ -36,7 +32,9 @@ function SimpleExpansionPanel(props) {
     <div className={classes.root}>
       <ExpansionPanel defaultExpanded={true}>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <img src={ProfileQualificationsImg} alt='qualifications' className={classes.imgPadding}/>
+          <svg style={{ width:24, height:24, viewBox:"0 0 24 24" }} className={classes.imgPadding}>
+              <path fill="#FF7043" d="M15.5,12C18,12 20,14 20,16.5C20,17.38 19.75,18.21 19.31,18.9L22.39,22L21,23.39L17.88,20.32C17.19,20.75 16.37,21 15.5,21C13,21 11,19 11,16.5C11,14 13,12 15.5,12M15.5,14A2.5,2.5 0 0,0 13,16.5A2.5,2.5 0 0,0 15.5,19A2.5,2.5 0 0,0 18,16.5A2.5,2.5 0 0,0 15.5,14M10,4A4,4 0 0,1 14,8C14,8.91 13.69,9.75 13.18,10.43C12.32,10.75 11.55,11.26 10.91,11.9L10,12A4,4 0 0,1 6,8A4,4 0 0,1 10,4M2,20V18C2,15.88 5.31,14.14 9.5,14C9.18,14.78 9,15.62 9,16.5C9,17.79 9.38,19 10,20H2Z" />
+          </svg>
           <Typography className={classes.heading}>Profile of Qualifications</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
@@ -47,7 +45,9 @@ function SimpleExpansionPanel(props) {
       </ExpansionPanel>
       <ExpansionPanel defaultExpanded={true} >
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <img src={TechnicalSkillsImg} alt='techskills' className={classes.imgPadding} />
+          <svg style={{ width:24, height:24, viewBox:"0 0 24 24" }} className={classes.imgPadding}>
+            <path fill="#FF7043" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
+          </svg>
           <Typography className={classes.heading}>Technical Skills</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
@@ -58,7 +58,9 @@ function SimpleExpansionPanel(props) {
       </ExpansionPanel>
       <ExpansionPanel defaultExpanded={true}>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-           <img src={WorkExpImg} alt='workexperience' className={classes.imgPadding} />
+          <svg style={{ width:24, height:24, viewBox:"0 0 24 24" }} className={classes.imgPadding}>
+            <path fill="#FF7043" d="M10,2H14A2,2 0 0,1 16,4V6H20A2,2 0 0,1 22,8V19A2,2 0 0,1 20,21H4C2.89,21 2,20.1 2,19V8C2,6.89 2.89,6 4,6H8V4C8,2.89 8.89,2 10,2M14,6V4H10V6H14Z" />
+          </svg>
           <Typography className={classes.heading}>Work Experience</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
@@ -69,7 +71,9 @@ function SimpleExpansionPanel(props) {
       </ExpansionPanel>
       <ExpansionPanel defaultExpanded={true}>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <img src={EducationImg} alt='education' className={classes.imgPadding} />
+          <svg style={{ width:24, height:24, viewBox:"0 0 24 24" }} className={classes.imgPadding}>
+            <path fill="#FF7043" d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,17.18V13.18L12,17L5,13.18Z" />
+          </svg>
           <Typography className={classes.heading}>Education</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>

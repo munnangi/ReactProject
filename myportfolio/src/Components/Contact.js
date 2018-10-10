ac3939235ee29cab79b6fec0334f3a6a16e7c2eb
munnangi <munnangi.prasanna@gmail.com>
2018-10-09 22:56:48 -0400

    changes to contact js file

--- myportfolio/src/Components/Contact.js
+++ myportfolio/src/Components/Contact.js
@@ -37,7 +37,7 @@ class Contact extends React.Component {
               <path fill="#FF7043" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
             </svg>
           </ListItemIcon>
-          <ListItemText className={classes.listItemStyle} primary="Gainesville, FL" />
+          <ListItemText className={classes.listItemStyle} primary="Gainesville, FL (willing to relocate)" />
         </ListItem>
         <ListItem >
           <ListItemIcon>

9326d4f65604792d269b892707f9dc9614628f58
munnangi <munnangi.prasanna@gmail.com>
2019-02-10 23:15:47 -0500

    Updates

---
+++ precache-manifest.c86a4b922c15fd69f40d465a30e5c3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d6a0da92c2e476e503be74991be143a",
+    "url": "/prasanna/static/media/pp1.3d6a0da9.jpeg"
+  },
+  {
+    "revision": "46d0214fec7067af8b11",
+    "url": "/prasanna/static/js/runtime~main.46d0214f.js"
+  },
+  {
+    "revision": "0b3ea7e944a59a80844b",
+    "url": "/prasanna/static/js/main.0b3ea7e9.chunk.js"
+  },
+  {
+    "revision": "8838dcd5e092e8ee73c8",
+    "url": "/prasanna/static/js/1.8838dcd5.chunk.js"
+  },
+  {
+    "revision": "0b3ea7e944a59a80844b",
+    "url": "/prasanna/static/css/main.84d6b32c.chunk.css"
+  },
+  {
+    "revision": "039218acdd07592a3eb772beb949d172",
+    "url": "/prasanna/index.html"
+  }
+];
